7fad94b8b606395dcea9ce2b2c4536bc92d79e35
mebi01 <53075163+mebi01@users.noreply.github.com>
2019-07-20 16:06:30 -0700

    Add files via upload

---
+++ 03_03/pipes-and-other-arguments.R
@@ -0,0 +1,12 @@
+library("praise")
+library("tidyverse")
+data <- c(1, 3, 5, 7, 11, 13, 17)
+
+rep(praise(), 10)
+
+rep(praise(), mean(diff(data)))
+
+
+midwest %>% 
+  select(state) %>%
+  unique()
